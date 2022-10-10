49f81f3877aef47fadae6c0799b276c204593190
Zhanna <zhanna01992@gmail.com>
2022-10-10 23:48:15 +0300

    feat: add testimonials and animals sliders, testimonials popup

---
+++ online-zoo/pages/donate/donate.js
@@ -0,0 +1,31 @@
+const input = document.querySelector('.interactive__amount')
+const radios = document.querySelectorAll('.linear__input')
+
+radios.forEach((item) => {
+  item.addEventListener('change', updateRadioAmount)
+})
+
+function updateRadioAmount(event) {
+  input.value = event.target.value
+}
+
+const existing = {
+  5000: 0,
+  2000: 1,
+  1000: 2,
+  500: 3,
+  250: 4,
+  100: 5,
+  50: 6,
+  25: 7,
+}
+
+const empty = document.getElementById('amount-0')
+input.addEventListener('keyup', (event) => {
+  const index = existing[event.target.value]
+  if (index !== undefined && radios[index]) {
+    radios[index].checked = true
+  } else {
+    empty.checked = true
+  }
+})
--- online-zoo/pages/main/petstory.js
+++ online-zoo/pages/main/petstory.js
@@ -1,10 +1,75 @@
-const burger = document.querySelector('.header__burger')
-const menu = document.querySelector('.header__nav')
-
-burger.addEventListener('click', showBurger)
-
-function showBurger() {
-  burger.classList.toggle('header__burger-active')
-  menu.classList.toggle('header__nav')
-  menu.classList.toggle('header__nav-active')
+const animalsCardsOrig = document.querySelector('.animals__cards')
+for (let i = 0; i < 5; i += 1) {
+  const animalsCards = animalsCardsOrig.cloneNode(true)
+  for (let j = animalsCards.children.length; j >= 0; j -= 1) {
+    animalsCards.appendChild(animalsCards.children[(Math.random() * j) | 0])
+  }
+  animalsCardsOrig.parentElement.appendChild(animalsCards)
 }
+
+// ________Swiper-slider Animals_________
+
+const animalsSwiper = new Swiper('.animals-slider', {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.card__btn-right',
+    prevEl: '.card__btn-left',
+  },
+  spaceBetween: 30,
+  loop: true,
+  speed: 800,
+})
+
+// Testimonials popUp
+
+const testPopup = document.querySelector('.testimonials__popup')
+const crossPopup = document.querySelector('.popup__close')
+const popupContent = document.querySelector('.popup_wrapper')
+const testItems = document.querySelectorAll('.testimonials__item')
+
+testItems.forEach((item) => {
+  item.addEventListener('click', openPopup)
+})
+
+crossPopup.addEventListener('click', closePopup)
+testPopup.addEventListener('click', closePopup)
+
+function openPopup(event) {
+  testPopup.classList.toggle('seePopup')
+  document.body.classList.toggle('has-overlay')
+  let html = ''
+  if (event.target.classList.contains('testimonials__item')) {
+    html = event.target.outerHTML
+  } else {
+    html = event.target.closest('.testimonials__item').outerHTML
+  }
+  popupContent.innerHTML = html
+}
+
+function closePopup() {
+  testPopup.classList.remove('seePopup')
+  document.body.classList.remove('has-overlay')
+}
+
+// // Swiper-slider Testimonials
+
+const testimonialsSlider = new Swiper('.testimonials-slider', {
+  spaceBetween: 15,
+  snapOnRelease: true,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+    dragSize: 50,
+  },
+  breakpoints: {
+    1600: {
+      slidesPerView: 4,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+    640: {
+      slidesPerView: 1,
+    },
+  },
+})
